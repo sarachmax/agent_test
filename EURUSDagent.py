51f338b88228ff1191d117a32fc6edcc59fec541
Sarach rujiranurak <sarach35@gmail.com>
2018-05-29 13:49:31 +0700

    add model

---
+++ EURUSDagent.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 28 21:00:56 2018
+
+@author: sarac
+"""
+import random
+import numpy as np
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras import backend as K
+from keras.layers import LSTM
+
+
+class DQNAgent:
+    def __init__(self, state_size):
+        self.memory = deque(maxlen=4000)
+        self.learning_rate = 0.001
+        self.state_size = state_size
+        self.epsilon = 1.0  # exploration rate
+        self.gamma = 0.95   # discount rate 
+        self.action_size = 3 
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.99
+        
+        self.model = self._build_model()
+        self.target_model = self._build_model()
+        
+    
+    
+    def _build_model(self):
+        # Neural Net for Deep-Q learning Model
+        model = Sequential()
+        model.add(LSTM(30, return_sequences = True, input_shape = (self.state_size,1), activation='relu'))
+        model.add(LSTM(18, return_sequences = True,  activation='relu'))
+        model.add(LSTM(6, activation='relu'))
+        model.add(Dense(3, activation='linear'))
+        model.compile(loss='mean_squared_error', optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def update_target_model(self):
+        # copy weights from model to target_model
+        self.target_model.set_weights(self.model.get_weights())
+    
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+   
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0]) #return action 
+    
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        
+        for state, action, reward, next_state, done in minibatch:
+            target = self.model.predict(state)
+            if done :
+                target[0][action] = reward
+            else : 
+                t = self.target_model.predict(next_state)[0]
+                target[0][action] = reward + self.gamma*np.amax(t)
+                
+            self.model.fit(state, target, epochs=1, verbose=0)
+        
+        if self.epsilon > self.epsilon_min : 
+            self.epsilon *= self.epsilon_decay
+        
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
+        
+    
---
+++ train.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 28 22:11:15 2018
+
+@author: sarac
+"""
+
+from EURUSDagent import DQNAgent
+import datetime
+import numpy as np
+import pandas as pd 
+
+EPISODES = 200
+
+start_index = 45    #2010.01.01 00:00
+end_index = 3161+1  #2012.12.30 20:00
+dataset = pd.read_csv('EURUSD_4H.csv')
+train_data = dataset.iloc[start_index:end_index,5:6]
+
+train_data = np.array(train_data)
+state_size = 60
+X_train = [] 
+for i in range(state_size, end_index-start_index):
+    X_train.append(train_data[i-state_size:i,0])
+X_train = np.array(X_train)
+
+
+class TrainEnvironment:
+    def __init__(self, data, num_index):
+        self.train_data = data
+        self.train_index = 0 
+        self.end_index = num_index
+        self.loss_limit = 0.1 
+        self.profit = 0.0 
+        
+    def reset(self):
+        self.train_index = 0 
+        self.profit = 0
+        return [self.train_data[self.train_index]]
+    
+    def get_action(self,action):
+        if action == 0 :
+            # buy 
+            return 1
+        elif action == 1 : 
+            # sell 
+            return -1
+        else : 
+            # noaction 
+            return 0 
+        
+    
+    def calculate_reward(self, action):
+        price_diff = self.train_data[self.train_index,59:60] - self.train_data[self.train_index,58:59]
+        self.profit += price_diff
+        reward = 0
+        action = self.get_action(action)
+        if price_diff*action > 0 :
+            reward = price_diff
+        elif price_diff*action < 0 : 
+            reward = 3*price_diff
+        elif price_diff == 0 and action == 0 : 
+            reward = 0.0001
+        return reward 
+    
+    
+    def done_check(self):
+        loss = -self.loss_limit*self.train_data[self.train_index,59:60]
+        print('loss limit : ', loss)
+        if self.train_index + 1 == self.end_index :
+            return True 
+        else :
+            return self.profit <= loss
+        
+    def step(self,action):
+        self.train_index += 1 
+        done = self.done_check()
+        ns = [X_train[self.train_index]]
+        reward = self.calculate_reward(action)
+        return ns, reward, done
+        
+    
+if __name__ == "__main__":
+    
+    agent = DQNAgent(state_size)
+    #agent.load("agent_model.h5")
+    num_index = end_index - start_index
+    env = TrainEnvironment(X_train, num_index)
+    batch_size = 32 
+    for e in range(EPISODES):
+        state = env.reset()
+        state = np.reshape(state, (1, state_size, 1))
+        
+        for t in range(end_index-start_index):
+            start_time = str(datetime.datetime.now().time())
+            action = agent.act(state)
+            next_state, reward, done = env.step(action)
+            next_state = np.reshape(next_state, (1,state_size,1))
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state       
+            if done:
+                agent.update_target_model()
+                print("episode: {}/{}, score: {}, e: {:.2}"
+                      .format(e, EPISODES, t, agent.epsilon))
+                break
+            
+            if len(agent.memory) > batch_size:
+                agent.replay(batch_size)
+            
+            end_time = str(datetime.datetime.now().time())
+              
+            print('-------------------- Check -------------------------')
+            print('start time: ' + start_time)  
+            print('counter : ', t,'/', end_index-start_index)
+            print('action : ', env.get_action(action))
+            print('reward : ', reward)
+            print('current profit : ', env.profit)
+            print('end_time: ' + end_time)
+            print('-------------------End Check -----------------------')
+            
+    agent.save("agent_model.h5")
+                      
+    
